bdcae37d581e23b40d3f3a96a3fa198481ac1a33
loganr01 <123439164+loganr01@users.noreply.github.com>
2025-04-30 14:02:18 -0500

    Update TheCountDoughWeb.py

--- TheCountDoughWeb.py
+++ TheCountDoughWeb.py
@@ -19,7 +19,7 @@ HTML_TEMPLATE = """
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>MOMS Badass Batch Calc</title>
+  <title>Batch Calc</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     body {
